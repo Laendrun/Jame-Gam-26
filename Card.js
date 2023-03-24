0b7cd00c829a7411f25e0ab75860f124eaafbb75
Simon <simon.aeby@laendrun.ch>
2023-03-24 21:00:37 +0100

    Worked on a Card class

--- Card.js
+++ Card.js
@@ -1,12 +1,13 @@
 class Card
 {
-	constructor(img)
+	constructor(img, text)
 	{
 		this.pos = createVector();
 		this.pos.x = ((width / 2) - (img.width / 2));
 		this.pos.y = ((height / 2) - (img.height / 2));
 		this.visible = true;
 		this.img = img;
+		this.text = text;
 	}
 
 	setPos(x, y)
@@ -19,7 +20,28 @@ class Card
 	{
 		if (this.visible)
 		{
-			image(this.img, this.pos.x, this.pos.y);
+			fill(125, 125, 125);
+			stroke(125, 125, 125);
+			rect(160, 60, 400, 600);
+
+			fill(180, 180, 180);
+			stroke(180, 180, 180);
+			rect(220, 80, 280, 200);
+
+			textSize(10);
+			fill(255);
+			text(this.text, 220, 80, 280, 200);
+
+			fill(225, 225, 225);
+			// stroke(225, 225, 225);
+			// rect(220, 320, 280, 280);
+			image(this.img, 220, 320);
+
+
+			// textSize(10);
+			// fill(255);
+			// text(this.text, this.pos.x, this.pos.y);
+			// image(this.img, this.pos.x, this.pos.y);
 		}
 	}
 }
--- sketch.js
+++ sketch.js
@@ -4,19 +4,20 @@ let cards = [];
 
 function preload()
 {
-	img = loadImage('assets/img/card_1.png');
+	img = loadImage('assets/img/card280x280.png');
 }
 
 function setup()
 {
+	let text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet massa neque. Pellentesque ut commodo risus. Phasellus vel nulla et sem feugiat sagittis. Aliquam faucibus, purus vel rhoncus lobortis, lorem ante feugiat enim, at mattis lorem quam nec turpis. Morbi mauris dui, molestie ut sodales a, rutrum ac mauris. Phasellus erat elit, vehicula in dictum et, imperdiet ut elit. Pellentesque quis purus ligula. Etiam a eleifend felis. Ut velit leo, efficitur ut tincidunt vel, efficitur id velit. Cras mattis porttitor leo. Nam luctus odio sed augue aliquam accumsan. Integer tincidunt, ipsum quis maximus sollicitudin, ex odio sollicitudin dolor, id suscipit magna lectus quis dolor. Donec aliquet vestibulum dui ut aliquet. Curabitur sem neque, pharetra sit amet pulvinar sit amet, ornare vulputate purus.";
 	createCanvas(720, 720);
-	cards[0] = new Card(img);
+	cards[0] = new Card(img, text);
 }
 
 function draw()
 {
 	background(0);
 	cards[0].show();
-	cards[0].setPos(mouseX, mouseY);
+	//cards[0].setPos(mouseX, mouseY);
 	//image(img, (width / 2) - (img.width / 2), (height / 2) - (img.height / 2));
 }
